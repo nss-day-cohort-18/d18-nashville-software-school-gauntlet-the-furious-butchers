29fce15139a7fa810c48fffd9dd19917f53c9c19
Blaise Roberts <blaiseroberts@Blaises-MacBook-Pro-2.local>
2017-03-08 11:23:53 -0600

    first draft of racectrl

--- app/controllers/RaceCtrl.js
+++ app/controllers/RaceCtrl.js
@@ -2,8 +2,32 @@
 
 app.controller("RaceCtrl", function($scope, PlayerFactory, $routeParams) {
 
+$scope.isHuman = true;
+$scope.isElf = false;
+$scope.isDwarf = false;
+
+
 // Input for player.name[ng-model]
 // Define $scope.player = {} [Object.create(species)]
+$scope.player = Object.create(PlayerFactory.getPlayer);
+
+
+$scope.getHuman = function(){
+	$scope.player = Object.create(PlayerFactory.getHuman);
+}
+
+$scope.getElf = function(){
+	$scope.player = Object.create(PlayerFactory.getElf);
+	$scope.isElf = true;
+	$scope.isHuman = false;
+}
+
+$scope.getDwarf = function(){
+	$scope.player = Object.create(PlayerFactory.getDwarf);
+	$scope.isDwarf = true;
+	$scope.isHuman = false;
+}
+
 
 
 });
