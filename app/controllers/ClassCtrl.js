376569709f3f77118b2fda874f762dabb09e85a8
Blaise Roberts <blaiseroberts@Blaises-MacBook-Pro-2.local>
2017-03-08 14:30:31 -0600

    first ideas

--- app/controllers/ClassCtrl.js
+++ app/controllers/ClassCtrl.js
@@ -1,7 +1,17 @@
 'use strict';
 
-app.controller("ClassCtrl", function($scope, PlayerFactory, $routeParams) {
+app.controller("ClassCtrl", function($scope, GameFactory, ClassFactory) {
 
+$scope.player = GameFactory.getGamePlayer();
+console.log($scope.player);
+
+$scope.player = Object.create(ClassFactory.getPlayerClass());
+console.log($scope.player);
+
+$scope.getClass = function(){
+	console.log(this.item);
+	$scope.player = Object.create(ClassFactory.getClass());
+};
 
 
 });
