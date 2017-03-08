194b7aa0abe31d00932f3ab8c133a073ad1038b7
Blaise Roberts <blaiseroberts@Blaises-MacBook-Pro-2.local>
2017-03-08 13:40:47 -0600

    fixed some bugs, can add player now.

--- app/controllers/RaceCtrl.js
+++ app/controllers/RaceCtrl.js
@@ -12,27 +12,40 @@ $scope.isDwarf = false;
 
 // Input for player.name[ng-model]
 // Define $scope.player = {} [Object.create(species)]
-$scope.player = Object.create(PlayerFactory.getPlayer);
-
+$scope.player = Object.create(PlayerFactory.getHuman());
 
 $scope.getHuman = function(){
-	$scope.player = Object.create(PlayerFactory.getHuman);
+	$scope.player = Object.create(PlayerFactory.getHuman());
+	$scope.isDwarf = false;
+	$scope.isElf = false;
+	$scope.isHuman = true;
+	console.log($scope.player);
+
+
 }
 
 $scope.getElf = function(){
-	$scope.player = Object.create(PlayerFactory.getElf);
+	$scope.player = Object.create(PlayerFactory.getElf());
 	$scope.isElf = true;
 	$scope.isHuman = false;
+	$scope.isDwarf = false;
+	console.log($scope.player);
+
+
 }
 
 $scope.getDwarf = function(){
-	$scope.player = Object.create(PlayerFactory.getDwarf);
+	$scope.player = Object.create(PlayerFactory.getDwarf());
 	$scope.isDwarf = true;
 	$scope.isHuman = false;
+	$scope.isElf = false;
+	console.log($scope.player);
+
+
 }
 
 
-$scope.setPlayer = function($scope.player){
+$scope.setPlayer = function(){
 	GameFactory.setGamePlayer($scope.player);
 }
 
--- app/factories/ClassesFactory.js
+++ app/factories/ClassesFactory.js
@@ -2,16 +2,16 @@
 
 app.factory("ClassesFactory", () => {
 
-  generateClass: function() {
-    // Get a random index from the allowed classes array
-    var random = Math.round(Math.random() * (this.allowedClasses.length - 1));
-    // Get the string at the index
-    var randomClass = this.allowedClasses[random];
-    // Composes the corresponding player class into the player object
-    this.class = new Gauntlet.GuildHall[randomClass]();
-    // Add the health bonus
-    this.health += this.class.healthBonus;
-    return this.class;
-  }
+  // generateClass: function() {
+  //   // Get a random index from the allowed classes array
+  //   var random = Math.round(Math.random() * (this.allowedClasses.length - 1));
+  //   // Get the string at the index
+  //   var randomClass = this.allowedClasses[random];
+  //   // Composes the corresponding player class into the player object
+  //   this.class = new Gauntlet.GuildHall[randomClass]();
+  //   // Add the health bonus
+  //   this.health += this.class.healthBonus;
+  //   return this.class;
+  // }
 
 });
