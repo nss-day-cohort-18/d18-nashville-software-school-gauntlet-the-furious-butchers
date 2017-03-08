50e08e6eeb21ad4fb753b7462da81e3709cbc9d2
James Tonkin <jameswtonkin@gmail.com>
2017-03-08 13:03:26 -0600

    created PlayerFactory and GameFactory

--- app/controllers/RaceCtrl.js
+++ app/controllers/RaceCtrl.js
@@ -2,8 +2,8 @@
 
 app.controller("RaceCtrl", function($scope, PlayerFactory, $routeParams) {
 
-// Input for player.name[ng-model]
-// Define $scope.player = {} [Object.create(species)]
+  
+
 
 
 });
