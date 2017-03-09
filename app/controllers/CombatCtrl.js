2b81e85f14a478e3035b8f324ec9f0649767a6fe
Blaise Roberts <blaiseroberts@Blaises-MacBook-Pro-2.local>
2017-03-09 16:15:33 -0600

    needs better logic

--- app/controllers/CombatCtrl.js
+++ app/controllers/CombatCtrl.js
@@ -1,6 +1,10 @@
 'use strict';
 
-app.controller("CombatCtrl", function($scope, ClassesFactory, PlayerFactory, SpellbookFactory, WeaponFactory, GameFactory, $routeParams) {
+app.controller("CombatCtrl", function($scope, ClassesFactory, PlayerFactory, SpellbookFactory, WeaponFactory, GameFactory, $route, $window) {
+///////////////////
+//Initialize Modals
+///////////////////
+  
 
   $scope.player = GameFactory.getGamePlayer();
   $scope.monster = PlayerFactory.getMonster();
@@ -10,6 +14,7 @@ app.controller("CombatCtrl", function($scope, ClassesFactory, PlayerFactory, Spe
   var turn = 1;
   var playerDamage = 0;
   var monsterDamage = 0;
+  console.log(turn);
 
   $scope.monster.randomMonClass = function() {
     // Get a random index from the allowed classes array
@@ -51,22 +56,55 @@ app.controller("CombatCtrl", function($scope, ClassesFactory, PlayerFactory, Spe
 
   var playerAttack = function() {
     if ($scope.player.class.magical) {
-      playerDamage = $scope.player.weapon.damage + ($scope.player.intelligence / 20);
+      playerDamage = Math.floor(Math.floor(Math.random() * $scope.player.weapon.damage) + ($scope.player.intelligence / 20));
     } else {
-      playerDamage = $scope.player.weapon.damage + ($scope.player.strength / 20);
+      playerDamage = Math.floor(Math.floor(Math.random() * $scope.player.weapon.damage) + ($scope.player.strength / 20));
     }
     $scope.monster.health -= playerDamage;
+    var playerMessage = `<p class="flow-text">${$scope.player.playerName} the ${$scope.player.class.name} attacked with their ${$scope.player.weapon.name} and did ${playerDamage} damage!</p>`;
+    $("#combat-log").prepend(playerMessage);
   };
 
   var monsterAttack = function() {
     if ($scope.monster.class.magical) {
-      monsterDamage = $scope.monster.weapon.damage + ($scope.monster.intelligence / 20);
+      monsterDamage = Math.floor(Math.floor(Math.random() * $scope.monster.weapon.damage) + ($scope.monster.intelligence / 20));
     } else {
-      monsterDamage = $scope.monster.weapon.damage + ($scope.monster.strength / 20);
+      monsterDamage = Math.floor(Math.floor(Math.random() * $scope.monster.weapon.damage) + ($scope.monster.strength / 20));
     }
     $scope.player.health -= monsterDamage;
+    var monsterMessage = `<p class="flow-text">${$scope.monster.species} the ${$scope.monster.class.name} attacked with their ${$scope.monster.weapon.name} and did ${monsterDamage} damage!</p>`;
+    $("#combat-log").prepend(monsterMessage);
   };
 
+  $scope.makeAttack = function(){
+    console.log("you made an attack");
+    playerAttack();
+    monsterAttack();
+    if ($scope.player.health <= 0){
+      console.log("you lose");
+       $('#modalLose').modal('show');
+    }
+    if ($scope.monster.health <= 0){
+      console.log("you win");
+       $('#modalWin').modal('show');
+    }
+  };
 
+  $scope.startOver = function(){
+    console.log("StartOver");
+    $('#modalLose').modal('hide');
+    $('#modalWin').modal('hide');
+    $window.location.href = "#!/";
+  };
+  $scope.keepGoing = function(){
+    console.log("keepGoing");
+    $('#modalLose').modal('hide');
+    $('#modalWin').modal('hide');
+    turn++;
+    $route.reload();
+
+
+
+  };
 
 });
